59438b931b845a6a9ac21f6fe3da4d3331530902
wanjiku <annritawanjiku77@gmail.com>
2022-09-24 11:07:10 +0300

    completed assignment

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,6 +2,7 @@ import React from "react";
 
 function MovieCard({
   title,
+  //adding default value in case image prop does not appear
   posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
   genres,
 }) {
